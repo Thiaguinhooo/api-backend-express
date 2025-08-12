34ae09807bacbf6e20ae259300b96e7e017d04ff
Pablo Pires <pablo.pires@aluno.ifsp.edu.br>
2025-08-12 17:50:37 -0300

    cria crud do profile

--- src/server.js
+++ src/server.js
@@ -1,13 +1,35 @@
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = 3333
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json()) //lendo as strings e voltando para 
+
+
+app.post('/user', (req, res) => {
+  const dados = req.body
+  res.send({message: 'usuario criado com sucesso!',
+    profile: dados
+  })
+})
+
+app.get('/user', (req, res) => {
+  res.json({message: 'Usuário consultado com sucesso!'})
+})
+
+app.put('/user', (req, res) => {
+  const dados = req.body
+  res.json({message: 'Usuário editado com sucesso!'})
+})
+
+app.patch('/user', (req, res) => {
+  res.json({message: 'Usuário editado parcialmente com sucesso!'})
+})
+
+app.delete('/user', (req, res) => {
+  res.json({message: 'Usuário deletado com sucesso'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`API Rodando em http://localhost:${port}`)
 })
-
